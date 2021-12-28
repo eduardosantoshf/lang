c3946068cd94d16b4da8b6e1afd00fa31c7aa630
BrunosBastos <Brunosb@ua.pt>
2021-12-28 22:32:16 +0000

    merge

--- src/findlang.py
+++ src/findlang.py
@@ -7,40 +7,36 @@ class Findlang:
     def __init__(self, train, k, alpha):
         self.k = k
         self.alpha = alpha
-        self.languages = {
-                            'eng.utf8': 'English',
-                            'pt_pt.utf8': 'Portuguese',
-                            'spanish.utf8': 'Spanish'
-                        }
         self.train = train
-        self.bits = []
 
     def find(self, target):
-
+        """ Predicts the language for a given target file"""
+        
         files = glob.glob(f"{self.train}*")
+        bits = []
 
         # for each of the languages calculate the number of bits
         for f in files:
             en = Fcm(f , self.k, self.alpha, 0)
             l = Lang(en)
             total_bits = l.compare_files(target)[0]
-            self.bits.append(total_bits)
+            bits.append(total_bits)
 
         # get the lower amount of bits
-        lang = files[self.bits.index(min(self.bits))] 
+        lang = files[bits.index(min(bits))] 
 
-        #lang_name = self.languages[lang.rsplit("/", 1)[1]]
+        lang_name = lang.split("/")[-1].split(".")[0]
 
         # returns the name of the file that has the lower number of needed bytes to encode
-        #return lang_name
-        return lang
+        return lang_name
 
     def find_all(self, target_folder):
+        """ Calculates the top 5 predicted language for each target in the target_folder"""
         
         targets = glob.glob(f"{target_folder}*.utf8")
         train = glob.glob(f"{self.train}*")
         
-        target_m = [None for _ in range(len(targets))]
+        target_m = [[] for _ in range(len(targets))]
         for i, tr in enumerate(train):
             
             en = Fcm(tr, self.k, self.alpha, 0)
@@ -48,14 +44,17 @@ class Findlang:
             
             for j, ta in enumerate(targets):
                 total_bits = l.compare_files(ta)[0]
-                if not target_m[j] or target_m[j][0] > total_bits:
-                    target_m[j] = (total_bits, tr)
+                target_m[j].append((total_bits, tr))
             
-        return {targets[i]:target_m[i][1] for i in range(len(targets))}
+        res = {}
+        for i, x in enumerate(target_m):
+            res[targets[i]] = [l[1] for l in sorted(target_m[i])[:5]]
             
+        return res
+                
 if __name__ == "__main__":
 
-    # python3 findlang.py -r "../langs/train/" -t "../langs/test/test_english.utf8"
+    # python3 findlang.py -r "../langs/train/" -t "../langs/test/english.utf8"
 
     parser = argparse.ArgumentParser(description='Lang')
     parser.add_argument("-r","--reference", type=str, required=True)
@@ -69,9 +68,6 @@ if __name__ == "__main__":
 
     train_folder = args["reference"]
 
-    #files = glob.glob("../langs/test/*")
-
     f = Findlang(train_folder, args["k"], args["alpha"])
-    print(f.find_all("../langs/test/"))
-    #language = f.find(args["target"])
-    #print("The selected text is predicted to have the " + language + " language.")
+    language = f.find(args["target"])
+    print("The selected text is predicted to have the " + language + " language.")
